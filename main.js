c761ef4938c3a93bee289061ca96147e07523158
Tyede <80978137+Tyede@users.noreply.github.com>
2021-03-19 15:05:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const RPC = require("discord-rpc");
+const { DiscordAPIError, Channel } = require("discord.js");
+const rpc = new RPC.Client({
+    transport: "ipc"
+});
+
+//Discord app üzerinde çalışır!
+
+rpc.on("ready", () => {
+    rpc.setActivity({
+        details: "This is Discord!",
+        state: "WoooW Interesting!",
+        startTimestamp: new Date(),
+        largeImageKey: "dc_icon",
+        largeImageText: "Discord!",
+//        smallImageKey: "discord_red",
+        smallImageText: "Discord!"
+    });
+
+    console.log("Başlatılıyor...");
+    console.log("Başlatıldı!");
+    console.log("Made by Developer/Software Impovrer(Tyede)!");
+});
+
+rpc.login({
+    clientId: "822514237556654101"
+});
